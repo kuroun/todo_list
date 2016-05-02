e45668edf0c2af09f31774502fd0be1b76fb4fc3
kuroun <roun.sk@gmail.com>
2016-05-01 23:49:45 -0500

    Revert "add scope, select all items belong to current user"
    This reverts commit 46f22dc68313151518517d5ef2135571bc1552ed.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
 	belongs_to :list
 	validates :title, :list_id, :presence => true
-	scope :all_items, -> (id) {where("list_id IN (?)", List.select(:id).where("user_id =?", id))}
 end
